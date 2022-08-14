b34f8713660c465506e2e14e70ba2cc1a92ddd8e
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-08-14 15:09:26 +0200

    fix: buttton type add

--- src/Types/types.ts
+++ src/Types/types.ts
@@ -14,6 +14,7 @@ export interface ButtonTypes {
 export interface ThemeType {
 	mainColor: string;
 	themeColor: string;
+	buttonSecondaryHover: string;
 	backgroundBack: string;
 	textGrey: string;
 	colorError: string;
