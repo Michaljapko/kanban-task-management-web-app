e864140c7da4bf1ef1938182addf9adb00af7c60
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-09-22 18:53:30 +0200

    fix CrossIcon type

--- src/components/UI/CrossIcon/CrossIcon.tsx
+++ src/components/UI/CrossIcon/CrossIcon.tsx
@@ -1,7 +1,9 @@
 import { StyledCrossIcon } from './CrossIcon.styled';
 
-const CrossIcon = ({ onClick } : {onClick: () => void }) => {
-	return <StyledCrossIcon onClick={onClick} />;
-};
+const CrossIcon = ({
+	onClick,
+}: {
+	onClick: React.MouseEventHandler<SVGSVGElement>;
+}) =>  <StyledCrossIcon onClick={onClick} />
 
 export default CrossIcon;
